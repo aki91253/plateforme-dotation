200dfc07848dbe0d9d189475cc3a04ca28220cb7
Stagiaire <Stagiaire@PC-DELOP3040-LWG2.crdp-corse.fr>
2026-01-06 09:26:14 +0100

    animation ipad

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ include 'includes/header.php';
     <section class="hero-section">
         <h1 class="hero-title">Votre plateforme<br>solidaire</h1>
         
-        <div class="hero-image-container">
-            <!-- Placeholder for the illustration -->
-            <img src="https://via.placeholder.com/900x500/E0E8E3/3A6B56?text=Illustration+R%C3%A9seau+Canop%C3%A9" alt="Illustration Solidaire">
-            <div style="position: absolute; top: 20%; left: 10%; text-align: left;">
-                <h2 style="color: #008080; font-size: 2rem;">Réseau<br>Canopé</h2>
+        <div class="ipad-wrapper">
+            <div class="ipad-device">
+                <div class="ipad-screen">
+                    <img src="assets/img/hero-illustration.png" alt="Réseau Canopé Illustration">
+                </div>
             </div>
         </div>
     </section>
