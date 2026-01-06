a0615eab715d45634cb529b1852723d9878d8d63
Jean Mathieu Lerda <jeanmathieulerda@MacBook-Pro-de-Jean.local>
2026-01-06 11:37:51 +0100

    modification du titre

--- contact.php
+++ contact.php
@@ -20,6 +20,8 @@ include 'includes/header.php';
             </form>
         </div>
         
+
+        
         <div>
             <!-- Map Placeholder --><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.073876723538!2d8.75318227648099!3d41.93426387123558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12da6a32050d040b%3A0x5d1a1183b2ec787a!2sCANOPE%20Acad%C3%A9mie%20de%20Corse!5e0!3m2!1sfr!2sfr!4v1767691927215!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
