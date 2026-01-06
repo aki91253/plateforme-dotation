4f6c833dc6b3f88ce67edcf81c520cc3164408f7
Jean Mathieu Lerda <jeanmathieulerda@MacBook-Pro-de-Jean.local>
2026-01-06 14:36:42 +0100

    correction

--- includes/footer.php
+++ includes/footer.php
@@ -27,14 +27,24 @@
                         </svg>
                     </a>
 
-                    <!-- YouTube -->
-                    <a href="https://www.youtube.com/@crdpcorse" target="_blank"
-                       class="w-8 h-8 bg-red-600 rounded-lg flex items-center justify-center hover:bg-red-700 transition-all hover:scale-110">
-                        <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-                        </svg>
-                    </a>
-                </div>
+    <!-- YouTube -->
+    <a href="https://www.youtube.com/@crdpcorse" target="_blank">
+      <div
+        style="clip-path: url(#squircleClip)"
+        class="w-14 h-14 bg-gradient-to-br from-red-600 to-red-800 rounded-xl flex flex-col items-center justify-center shadow-lg border border-red-500/50 cursor-pointer transform transition-all duration-300 ease-out hover:scale-110 hover:-translate-y-2 hover:shadow-2xl"
+      >
+        <!-- Logo triangle -->
+        <svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-white mb-1" xmlns="http://www.w3.org/2000/svg">
+          <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+        </svg>
+      </div>
+    </a>
+
+  </div>
+</div>
+
+</div>
+
             </div>
         </div>
     </footer>
