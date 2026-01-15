5cad2d8a9086b6e05121a284698053db159c962d
Jean Mathieu Lerda <jeanmathieulerda@MacBook-Pro-de-Jean.local>
2026-01-15 09:31:14 +0100

    ajout commentaire main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,32 @@ console.log('Dotation Platform loaded');
 
 //-------------------
 //Méthode pour cart 
-
-
-function getCart() { // Méthode pour obtenir la sélection 
+//-------------------
+function getCart() { // 1.1 -- Méthode pour obtenir le panier
     const cart = localStorage.getItem('canope_cart');
     return cart ? JSON.parse(cart) : [];
 }
 
-function saveCart(cart) {
+function saveCart(cart) { //2.1 -- Méthode pour sauvegarder le panier quand on ajoute 
     localStorage.setItem('canope_cart', JSON.stringify(cart));
     updateCartCount();
 }
 
-function addToCart(productId, productName) {
+function addToCart(productId, productName) {//3.1 -- Méthode pour ajouter un produit dans le paniern (Id, nom du produit, quantité)
     const cart = getCart();
     const existingItem = cart.find(item => item.id === productId);
 
-    if (existingItem) {
+    if (existingItem) { // 3.2 -- Vérification si le produit est déjà présent dans notre panier
         showNotification(`"${productName}" est déjà dans votre sélection`);
         return;
     }
 
-    cart.push({ id: productId, name: productName, quantity: 1 });
+    cart.push({ id: productId, name: productName, quantity: 1 }); // 3.3 --  Sinon on ajoute le produit dans le panier 
     saveCart(cart);
     showNotification(`"${productName}" ajouté à votre sélection`);
 }
 
-function removeFromCart(productId) {
+function removeFromCart(productId) {//4.1 -- Méthode pour supprimer un produit du panier ( La croix )
     let cart = getCart();
     cart = cart.filter(item => item.id !== productId);
     saveCart(cart);
@@ -36,95 +35,39 @@ function removeFromCart(productId) {
     if (typeof displayCart === 'function') displayCart();
 }
 
-function updateQuantity(productId, newQuantity) {
+function updateQuantity(productId, newQuantity) { // 5.1 -- Méthode pour mettre à jour la quantité du panier lors d'un "+" ou "-"
     const cart = getCart();
     const item = cart.find(item => item.id === productId);
 
-    if (item) {
-        if (newQuantity <= 0) removeFromCart(productId);
-        else { item.quantity = newQuantity; saveCart(cart); }
+    if (item) { // 5.2 -- Vérifie si l'item existe 
+        if (newQuantity <= 0) removeFromCart(productId); // 5.3 --  Vérifie si la quantité ne descend pas en dessous de 0 ou égale 0 --> alors suppression du produit
+        else { item.quantity = newQuantity; saveCart(cart); } // 5.4 --  Sinon on change la valeur de la quantité du produit avec la nouvelle 
     }
 
     if (typeof displayCart === 'function') displayCart();
 }
 
-function clearCart() {
+function clearCart() { // 6.1 -- Méthode pour vider entièrement le panier
     localStorage.removeItem('canope_cart');
     updateCartCount();
 }
 
-function updateCartCount() {
+function updateCartCount() { // 7.1 -- Méthode pour mettre à jour le nombre d'éléments du panier ( pour la petite bulle sur l'icone du panier )
     const cart = getCart();
     const totalItems = cart.length;
     const cartCountElement = document.getElementById('cart-count');
 
-    if (cartCountElement) {
-        if (totalItems > 0) {
+    if (cartCountElement) { // 7.2 -- Vérifie si l'élément existe 
+        if (totalItems > 0) { // 7.3 -- Vérifie si il y a au moins 1 élément
             cartCountElement.textContent = totalItems;
             cartCountElement.classList.remove('hidden');
-        } else {
+        } else { // 7.4 -- Sinon si le panier est vide cache la bulle 
             cartCountElement.classList.add('hidden');
         }
     }
 }
 
-//--------------------
-// Méthdode pour les favoris 
-//_------------------
 
-
-
-function getFav() {
-    const fav = localStorage.getItem('canope_fav');
-    return fav ? JSON.parse(fav) : [];
-}
-
-function saveFav(fav) {
-    localStorage.setItem('canope_fav', JSON.stringify(fav));
-    updateFavCount();
-}
-
-function addToFav(productId, productName) {
-    const fav = getFav();
-    const existingItem = fav.find(item => item.id === productId);
-
-    if (existingItem) {
-        showNotification(`"${productName}" est déjà dans vos favoris`);
-        return;
-    }
-
-    fav.push({ id: productId, name: productName });
-    saveFav(fav);
-    showNotification(`"${productName}" ajouté à vos favoris`);
-}
-
-function removeFromFav(productId) {
-    let fav = getFav();
-    fav = fav.filter(item => item.id !== productId);
-    saveFav(fav);
-
-    if (typeof displayFav === 'function') displayFav();
-}
-
-function clearFav() {
-    localStorage.removeItem('canope_fav');
-    updateFavCount();
-}
-
-function updateFavCount() {
-    const fav = getFav();
-    const totalItems = fav.length;
-    const favCountElement = document.getElementById('fav-count');
-
-    if (favCountElement) {
-        if (totalItems > 0) {
-            favCountElement.textContent = totalItems;
-            favCountElement.classList.remove('hidden');
-        } else {
-            favCountElement.classList.add('hidden');
-        }
-    }
-}
 
 // ====================
 // Notifications (Toast)
